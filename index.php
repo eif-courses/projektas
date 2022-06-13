f39f3bbce64f5e89c2dd6ce0312e237ba3e508a1
Marius Gžegoževskis <m.gzegozevskis@eif.viko.lt>
2022-06-13 08:40:56 +0300

    istrintas kodas

--- index.php
+++ index.php
@@ -19,15 +19,7 @@ $nameErr = $emailErr = $genderErr = $websiteErr = "";
 $name = $email = $gender = $comment = $website = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["name"])) {
-        $nameErr = "Name is required";
-    } else {
-        $name = test_input($_POST["name"]);
-        // check if name only contains letters and whitespace
-        if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
-            $nameErr = "Only letters and white space allowed";
-        }
-    }
+   
 
     if (empty($_POST["email"])) {
         $emailErr = "Email is required";
@@ -38,6 +30,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $emailErr = "Invalid email format";
         }
     }
+
+
+if (empty($_POST["website"])){}
 
     if (empty($_POST["website"])) {
         $website = "";
